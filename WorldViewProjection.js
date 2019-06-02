19f422822195733c3ec1f6e31450c6d559121930
Naomi Petrushevsky <naomi@Massimos-MacBook-Pro.local>
2019-06-02 14:42:21 +0200

    add airplane

--- WorldViewProjection.js
+++ WorldViewProjection.js
@@ -31,30 +31,8 @@ function worldViewProjection(carx, cary, carz, cardir, camx, camy, camz, aspectR
 	var view = utils.invertMatrix(viewinv);
 	var projection = [1/(aspectRatio*Math.tan(Math.PI/6)), 0, 0, 0,
 						0, 1/(Math.tan(Math.PI/6)), 0, 0,
-						0, 0, 5000.1/(0.1-5000), 2*0.1*5000/-4999.9,
+						0, 0, 10000.1/(0.1-10000), 2*0.1*10000/-9999.9,
 						0, 0, -1, 0];
 
 	return [world, view, projection];
-}
-
-
-
-
-// this function returns the world matrix with the updated rotations.
-// parameters rvx, rvy and rvz contains a value in the -1 .. +1 range that tells the angular velocity of the world.
-function updateWorld(rvx, rvy, rvz) {
-
-
-	Rrvx=utils.degToRad(rvx);
-	Rrvy=utils.degToRad(rvy);
-	Rrvz=utils.degToRad(rvz);
-
-
-	var rotationDirX = new Quaternion(Math.cos(Rrvx), Math.sin(Rrvx), 0, 0);
-	var rotationDirY = new Quaternion(Math.cos(Rrvy), 0, Math.sin(Rrvy), 0);
-	var rotationDirZ = new Quaternion(Math.cos(Rrvz), 0, 0, Math.sin(Rrvz));
-
-	currentPos= rotationDirX.mul(rotationDirY.mul(rotationDirZ.mul(currentPos)));
-
-	return currentPos;
 }
